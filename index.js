d2c440015d6b060a13ef553d0a0fe21dfc4f1791
Akshay Katariya <akshay.katariya1@gmail.com>
2021-07-11 09:44:19 +0530

    Deploy qr code

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
-
+const express = require("express");
+const { url } = require("inspector");
+const path = require("path");
+const PORT = process.env.PORT || 5000;
+var QRCode = require("qrcode");
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .use(express.static(path.join(__dirname, "public")))
+  .set("views", path.join(__dirname, "views"))
+  .set("view engine", "ejs")
+  .get("/", (req, res) => {
+    QRCode.toDataURL("I am a pony!", function (err, url) {
+      res.render("index", { qrCode: url });
+    });
+  })
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
