eece9105c62390638e60f9ae5880f0efbfc219e6
Akshay Katariya <akshay.katariya1@gmail.com>
2021-07-11 11:30:47 +0530

    WhatsApp integrated

---
+++ src/app.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 5000;
+var QRCode = require("qrcode");
+
+const { Client } = require("whatsapp-web.js");
+const client = new Client();
+const inshorts = require("inshorts-api");
+
+const {
+  hasNumber,
+  getCategoryName,
+  formatNews,
+  getFinalNews,
+  getDateTime,
+} = require("./utils/consant");
+const { defaultMessage } = require("./utils/default");
+
+let languageCode = "en";
+let isHindi = false;
+
+let newsOptions = {
+  lang: "en",
+  category: "national",
+  numOfResults: 10,
+};
+
+express()
+  .use(express.static(path.join(__dirname, "public")))
+  .set("views", path.join(__dirname, "views"))
+  .set("view engine", "ejs")
+  .get("/", (req, res) => {
+    client.on("qr", (qr) => {
+      // Generate and scan this code with your phone
+      console.log("QR RECEIVED", qr);
+      QRCode.toDataURL(qr, function (err, url) {
+        res.render("index", { qrCode: url, now: getDateTime() });
+      });
+    });
+  })
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+client.on("ready", () => {
+  console.log("Client is ready!");
+});
+
+client.on("message", (msg) => {
+  let message = msg.body.toLocaleLowerCase();
+  console.log(message);
+
+  languageCode = message.indexOf("hindi") !== -1 ? "hi" : "en";
+  if (message === "news") {
+    //Show intro msg
+    msg.reply(defaultMessage);
+  }
+  //check if news has category
+  if (message.includes("news") && hasNumber(message)) {
+    try {
+      console.log("languageCode=>", languageCode);
+      newsOptions.category = getCategoryName(message);
+      newsOptions.lang = languageCode;
+      console.log(JSON.stringify(newsOptions));
+      //call inshorts api
+      inshorts.get(newsOptions, function (result) {
+        msg.reply(getFinalNews(result));
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+});
+
+client.initialize();
---
+++ src/utils/consant.js
@@ -0,0 +1,73 @@
+module.exports.hasNumber = function (myString) {
+  return /\d/.test(myString);
+};
+
+const getCategoryNumber = function (str) {
+  let numb = str.match(/\d/g);
+  return numb.join("");
+};
+
+module.exports.getCategoryName = function (str) {
+  let num = Number(getCategoryNumber(str));
+  console.log("getCategoryNumber(str)==", num);
+  switch (num) {
+    case 1:
+      return "national ";
+    case 2:
+      return "business";
+    case 3:
+      return "sports";
+    case 4:
+      return "world";
+    case 5:
+      return "politics";
+    case 6:
+      return "technology";
+    case 7:
+      return "startup";
+    case 8:
+      return "entertainment";
+    case 9:
+      return "miscellaneous";
+    case 10:
+      return "hatke";
+    case 11:
+      return "science";
+    case 12:
+      return "automobile";
+    default:
+      return "";
+  }
+};
+
+let title;
+let content;
+let singleNews;
+
+module.exports.getFinalNews = function (newsResponse) {
+  let news = "";
+  newsResponse.forEach(function (element) {
+    title = element.title;
+    content = element.content;
+    singleNews = `${title} \n\n${content}`;
+    news = `${news} \n\n${singleNews}\n\n-----------`;
+  });
+  return news;
+};
+
+module.exports.getDateTime = function () {
+  let date = new Date();
+  let dateStr =
+    ("00" + (date.getMonth() + 1)).slice(-2) +
+    "/" +
+    ("00" + date.getDate()).slice(-2) +
+    "/" +
+    date.getFullYear() +
+    " " +
+    ("00" + date.getHours()).slice(-2) +
+    ":" +
+    ("00" + date.getMinutes()).slice(-2) +
+    ":" +
+    ("00" + date.getSeconds()).slice(-2);
+  return dateStr;
+};
