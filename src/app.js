e6c21b831e18a2137e79237e0da720c2f1298d64
Akshay Katariya <akshay.katariya1@gmail.com>
2021-07-11 19:17:03 +0530

    Added session management

--- src/app.js
+++ src/app.js
@@ -2,11 +2,25 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const PORT = process.env.PORT || 5000;
-var QRCode = require("qrcode");
+const QRCode = require("qrcode");
+const fs = require("fs");
 
 const { Client } = require("whatsapp-web.js");
-const client = new Client();
 const inshorts = require("inshorts-api");
+
+// Path where the session data will be stored
+const SESSION_FILE_PATH = "./session.json";
+
+// Load the session data if it has been previously saved
+let sessionData;
+if (fs.existsSync(SESSION_FILE_PATH)) {
+  sessionData = require(SESSION_FILE_PATH);
+}
+
+// Use the saved values
+const client = new Client({
+  session: sessionData,
+});
 
 const {
   hasNumber,
@@ -43,6 +57,16 @@ app.get("/qr", (req, res) => {
     QRCode.toDataURL(qr, function (err, url) {
       res.render("index", { qrCode: url, now: getDateTime() });
     });
+  });
+});
+
+// Save session values to the file upon successful auth
+client.on("authenticated", (session) => {
+  sessionData = session;
+  fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), function (err) {
+    if (err) {
+      console.error(err);
+    }
   });
 });
 
